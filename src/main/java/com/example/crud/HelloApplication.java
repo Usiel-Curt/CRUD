f7ad60019e0ff99336a70ae9aee9c6a219a20da7
Usiel-Curt <20243ds143@utez.edu.mx>
2025-08-10 22:58:26 -0600

    Se agrego css

--- src/main/java/com/example/crud/HelloApplication.java
+++ src/main/java/com/example/crud/HelloApplication.java
@@ -11,9 +11,9 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 720, 450);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 800, 470);
         stage.setScene(scene);
+        stage.setResizable(false);
         stage.show();
     }
 
